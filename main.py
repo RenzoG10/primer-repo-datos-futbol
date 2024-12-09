ed1ba05b09a8cb768dd1a9a4d28c68bc7b2419d5
Renzo Galetto <Renzo.galetto1007@gmail.com>
2024-12-09 11:34:48 -0300

    Cambios para obtener los minutos
    Cambio en el main sustituyendo por lo de prueba_minutos, cambios en obtencion de goles en vivo (probando)

--- main.py
+++ main.py
@@ -27,9 +27,9 @@ def detectar_estado(texto):
     if texto.endswith("TC"):
         return "Partido Finalizado"
     elif texto.endswith("Pen"):
-        return "Finalizo en tanda de Penales"
+        return "Finalizo en Pen."
     elif texto.endswith("AET"):
-        return "Finalizado en Tiempo Extra"
+        return "Finalizado en T.E."
     elif texto.endswith("ET"):
         return "Entre Tiempo"
     elif texto.endswith("Ap"):
@@ -40,6 +40,17 @@ def detectar_estado(texto):
         return "Cancelado"
 
     return None
+
+etiquetas_estado = ["TC", "Pen", "AET", "ET", "Ap", "Ab", "Ca"]
+
+def limpiar_nombre_equipo(nombre, etiquetas):
+    """
+    Limpia el nombre del equipo eliminando etiquetas específicas al final.
+    """
+    for etiqueta in etiquetas:
+        if nombre.endswith(etiqueta):
+            nombre = nombre[: -len(etiqueta)].strip()  # Elimina la etiqueta y cualquier espacio adicional
+    return nombre
 
 # Función principal para buscar partidos
 def buscar_partidos():
@@ -55,7 +66,7 @@ def buscar_partidos():
 
     equipo_buscado = input("\nIngrese el nombre del equipo a buscar (o escriba 'Todos' o 'Todo' para buscar todo): ").strip()
 
-    uservivo = input("Ingrese VIVO o NO JUGADOS o FINALIZADOS: ")
+    uservivo = input("\nIngrese VIVO o NO JUGADOS o FINALIZADOS: ")
 
     # Iniciar Selenium y BeautifulSoup
     driver = webdriver.Chrome(options=option)
@@ -81,6 +92,7 @@ def buscar_partidos():
             equipos1_div1 = partido.find("div", class_="css-9871a0-StatusAndHomeTeamWrapper e1ek4pst4")
             div2 = partido.find("div", class_="css-k083tz-StatusLSMatchWrapperCSS e5pc0pz0")
             equipos2_div3 = partido.find("div", class_="css-gn249o-AwayTeamAndFollowWrapper e1ek4pst5")
+            minutos11 = equipos1_div1.find("span", class_ = "css-doevad-StatusDotCSS e1yf8uo31")
 
             resultados = None
             if div2 is not None:
@@ -93,23 +105,24 @@ def buscar_partidos():
                 if resultados is None:
                     resultados = "Hora no encontrada"
 
+            minutos = minutos11.text.strip() if minutos11 else ""
             equipo1 = equipos1_div1.text.strip() if equipos1_div1 else "Equipo no encontrado"
             resultado = resultados.text.strip() if hasattr(resultados, 'text') else resultados
             equipo2 = equipos2_div3.text.strip() if equipos2_div3 else "Equipo no encontrado"
-
-            # Detectar minutos al final del nombre
-            minutos = detectar_minutos(equipo1)
+            
             estado = detectar_estado(equipo1)  # Detectar si es un TC, Pen, AET o ET
+
+            if estado is None:
+                minutos = minutos
+            else:
+                minutos = estado
 
             # Eliminar los minutos y estado del nombre del equipo
             if minutos:
                 equipo1 = equipo1.rstrip(minutos)
             if estado:
-                equipo1 = equipo1.rstrip(estado)
-
-            # Agregar los minutos y estado al lado de los minutos
-            if estado:
-                minutos = f"{minutos}{estado}"
+                equipo1 = limpiar_nombre_equipo(equipo1, etiquetas_estado)
+                equipo2 = limpiar_nombre_equipo(equipo2, etiquetas_estado)
 
             # Agregar el partido a la lista si coincide con el equipo buscado o si se selecciona "Todos"
             if equipo_buscado.lower() in equipo1.lower() or equipo_buscado.lower() in equipo2.lower():
@@ -119,8 +132,6 @@ def buscar_partidos():
                 partidos_de_liga.append((minutos, equipo1, resultado, equipo2))
                 partido_encontrado = True
             
-                
-                              
         ligas_y_partidos.append((titulo_liga, partidos_de_liga))
 
     # Mostrar resultados
@@ -139,9 +150,9 @@ def buscar_partidos():
                 for minutos, equipo1, resultado, equipo2 in partidos:
                     #print(f"{minutos:<10} {equipo1:<30} {resultado:<20} {equipo2:<30}")
                     if minutos != "":
-                        if minutos != "Partido Finalizado" and minutos != "Finalizo en tanda de Penales" and minutos != "Finalizado en Tiempo Extra" and minutos != "Aplazado" and minutos != "Cancelado" and minutos != "Suspendido":
+                        if minutos != "Partido Finalizado" and minutos != "Finalizo en Pen." and minutos != "Finalizado en T.E." and minutos != "Aplazado" and minutos != "Cancelado" and minutos != "Suspendido":
                             partidosenvivo.append((minutos, equipo1, resultado, equipo2))
-                        elif minutos == "Partido Finalizado" or minutos == "Suspendido" or minutos == "Finalizo en tanda de Penales" or minutos == "Finalizado en Tiempo Extra":
+                        elif minutos == "Partido Finalizado" or minutos == "Suspendido" or minutos == "Finalizo en Pen." or minutos == "Finalizado en T.E.":
                             partidosfinalizados.append((minutos, equipo1, resultado, equipo2))
                         elif minutos == "Cancelado" or minutos == "Aplazado":
                             partidosnojugados.append((minutos, equipo1, resultado, equipo2))
@@ -151,7 +162,7 @@ def buscar_partidos():
     if uservivo == "VIVO":
         print("Partidos en vivo:")
         for minutos, equipo1, resultado, equipo2 in partidosenvivo:
-            print(f"{minutos:<15} {equipo1:<30} {resultado:<20} {equipo2:<30}")
+            print(f"{minutos:<20} {equipo1:<30} {resultado:<20} {equipo2:<30}")
     
     # DETECCION DE GOLES Y DE COMIENZOS DE PARTIDOS
 
@@ -196,13 +207,16 @@ def buscar_partidos():
                             key = f"{equipo1} vs {equipo2}"
                             estado_actual[key] = resultado
                                                                         
-                            if key in estado_previos and estado_previos[key] == "0 - 0" and minutos == "1":
+                            '''if key in estado_previos and estado_previos[key] == "0 - 0" and minutos == "1":
+                                print(f"{minutos} Comenzo el partido {key}!: Marcador: {resultado}")'''
+                            
+                            if key not in estado_previos and estado_actual[key] == "0 - 0" and (minutos == "1" or minutos == "0"):
                                 print(f"{minutos} Comenzo el partido {key}!: Marcador: {resultado}")
 
                             
                             elif key in estado_previos and estado_previos[key] != resultado:
                                 print(f"{minutos} GOL en el partido {key}! Nuevo resultado: {resultado}")
-                                
+                                 
 
                 estado_previos = estado_actual
                 time.sleep(30)
--- prueba_minutos.py
+++ prueba_minutos.py
@@ -6,6 +6,38 @@ import time
 option = webdriver.ChromeOptions()
 option.add_argument("--headless")
 
+# Función para detectar si hay TC, Pen, AET o ET al final del nombre del equipo
+def detectar_estado(texto):
+    """
+    Detecta si el nombre del equipo tiene TC, Pen, AET o ET al final.
+    """
+    if texto.endswith("TC"):
+        return "Partido Finalizado"
+    elif texto.endswith("Pen"):
+        return "Finalizo en Pen."
+    elif texto.endswith("AET"):
+        return "Finalizado en T.E."
+    elif texto.endswith("ET"):
+        return "Entre Tiempo"
+    elif texto.endswith("Ap"):
+        return "Aplazado"
+    elif texto.endswith("Ab"):
+        return "Suspendido"
+    elif texto.endswith("Ca"):
+        return "Cancelado"
+
+    return None
+
+def limpiar_nombre_equipo(nombre, etiquetas):
+    """
+    Limpia el nombre del equipo eliminando etiquetas específicas al final.
+    """
+    for etiqueta in etiquetas:
+        if nombre.endswith(etiqueta):
+            nombre = nombre[: -len(etiqueta)].strip()  # Elimina la etiqueta y cualquier espacio adicional
+    return nombre
+
+etiquetas_estado = ["TC", "Pen", "AET", "ET", "Ap", "Ab", "Ca"]
 
 def buscar_partidos():
     # Solicitar fecha y país
@@ -44,6 +76,7 @@ def buscar_partidos():
             equipos1_div1 = partido.find("div", class_="css-9871a0-StatusAndHomeTeamWrapper e1ek4pst4")
             div2 = partido.find("div", class_="css-k083tz-StatusLSMatchWrapperCSS e5pc0pz0")
             equipos2_div3 = partido.find("div", class_="css-gn249o-AwayTeamAndFollowWrapper e1ek4pst5")
+            minutos11 = equipos1_div1.find("span", class_ = "css-doevad-StatusDotCSS e1yf8uo31")
 
             resultados = None
             if div2 is not None:
@@ -56,23 +89,27 @@ def buscar_partidos():
                 if resultados is None:
                     resultados = "Hora no encontrada"
 
+            minutos = minutos11.text.strip() if minutos11 else ""
             equipo1 = equipos1_div1.text.strip() if equipos1_div1 else "Equipo no encontrado"
             resultado = resultados.text.strip() if hasattr(resultados, 'text') else resultados
             equipo2 = equipos2_div3.text.strip() if equipos2_div3 else "Equipo no encontrado"
-
-            # Detectar minutos al final del nombre
-            minutos = detectar_minutos(equipo1)
+            
             estado = detectar_estado(equipo1)  # Detectar si es un TC, Pen, AET o ET
+
+            if estado is None:
+                minutos = minutos
+            else:
+                minutos = estado
 
             # Eliminar los minutos y estado del nombre del equipo
             if minutos:
                 equipo1 = equipo1.rstrip(minutos)
             if estado:
-                equipo1 = equipo1.rstrip(estado)
-
+                equipo1 = limpiar_nombre_equipo(equipo1, etiquetas_estado)
+                equipo2 = limpiar_nombre_equipo(equipo2, etiquetas_estado)
             # Agregar los minutos y estado al lado de los minutos
-            if estado:
-                minutos = f"{minutos}{estado}"
+            '''if estado:
+                minutos = f"{minutos}{estado}"'''
 
             # Agregar el partido a la lista si coincide con el equipo buscado o si se selecciona "Todos"
             if equipo_buscado.lower() in equipo1.lower() or equipo_buscado.lower() in equipo2.lower():
@@ -82,8 +119,6 @@ def buscar_partidos():
                 partidos_de_liga.append((minutos, equipo1, resultado, equipo2))
                 partido_encontrado = True
             
-                
-                              
         ligas_y_partidos.append((titulo_liga, partidos_de_liga))
 
     # Mostrar resultados
@@ -96,7 +131,6 @@ def buscar_partidos():
             if paisbuscadoarreglado == "Todos" or paisbuscadoarreglado == "Todo" or paisbuscadoarreglado in liga:
                 print(f"\nLiga: {liga} - Total de partidos: {len(partidos)}\n")
                 for minutos, equipo1, resultado, equipo2 in partidos:
-                    print(f"{minutos:<10} {equipo1:<30} {resultado:<20} {equipo2:<30}")
+                    print(f"{minutos:<20} {equipo1:<30} {resultado:<20} {equipo2:<30}")
 
-    #HACER LO DE LOS MINUTOS CON WEBSCRAPTING DIRECTAMENTE
-    #NO HAY NADA HECHO
+buscar_partidos()
