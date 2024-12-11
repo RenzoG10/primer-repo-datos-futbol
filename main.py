5b578cf7582ac2dd6a3c93eb2738ef65783fca63
Renzo Galetto <Renzo.galetto1007@gmail.com>
2024-12-11 20:52:26 -0300

    Goles y comienzo mejorado
    Andaba mal el anterior asi que lo traje al main para que ande por lo menos mejor, falta hacerle una función aparte para que la pueda llamar desde el main

--- main.py
+++ main.py
@@ -24,7 +24,39 @@ def main():
         for minutos, equipo1, resultado, equipo2 in partidosenvivo:
             print(f"{minutos:<20} {equipo1:<30} {resultado:<20} {equipo2:<30}")
     
-        goles_comienzo(fecha, equipo_buscado)
+        estado_previos = {}
+
+        print("\nMonitoreando cambios en los resultados. Presione Ctrl+C para detener.")
+
+        try:
+            while True:
+
+                estado_actual = {}
+
+                if equipo1 and equipo2 and resultado:
+                    key = f"{equipo1} vs {equipo2}"
+                    estado_actual[key] = (resultado, minutos)
+
+                    if key in estado_previos:
+                        resultado_anterior, minutos_anteriores = estado_previos[key]
+
+                        if minutos_anteriores == "" and minutos != "":
+                            print(f"{minutos} Comenzó el partido {key} con marcador: {resultado}")
+
+                        # Detectar goles
+                        if resultado_anterior != resultado:
+                            print(f"{minutos} GOL en el partido {key}! Nuevo resultado: {resultado}")
+
+                    elif minutos == "1" or minutos == "0":
+                            print(f"{minutos} Comenzó el partido {key} con marcador: {resultado}")
+
+                estado_previos = estado_actual
+                time.sleep(30)
+
+        except KeyboardInterrupt:
+            print("\nMonitoreo detenido por el usuario.")
+        finally:
+            driver.quit()
 
     elif uservivo == "NO JUGADOS":
         print("Partidos sin jugar todavia:")
